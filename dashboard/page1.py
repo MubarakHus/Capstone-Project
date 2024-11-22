ebafd82fa4ece9efeedd20e00892164a9cddd183
ghadaMhammed <ghadamhammed5@gmail.com>
2024-11-22 22:47:28 +0300

    style adjustment

--- dashboard/page1.py
+++ dashboard/page1.py
@@ -13,25 +13,54 @@ data_folder_path = os.path.join(parent_dir, "data")
 all_data=pd.read_csv(os.path.join(data_folder_path,"df_q_group.csv"))
 app = Dash(__name__) 
 
+
+# page 1: نظرة عامة على اتجاهات التوظيف
+
+# create the graph
 fig = px.line(
         all_data,
         x="Q",
         y="total_employees",
         color="year",
-        title="عدد الموظفين حسب السنة",
-        labels={"Q": "الربع", "total_employees": "عدد الموظفين", "year": "السنة"}
+    
+        labels={"Q": "الربع", "total_employees": "عدد الموظفين", "year": "السنة"},
+        markers=True
+
     )
-# 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-   Dash: A web application framework for your data. '''),
-
-    dcc.Graph(
-      id='empperyear',
-        figure=fig
-    )])
+# style the graph
+fig.update_layout(
+    title={
+        "text": "عدد الموظفين حسب السنة",
+        "x": 0.5, # center the title
+        "xanchor": "center",
+        "yanchor": "top",
+    },
+    font=dict(color="#333333"),
+    plot_bgcolor="#F5F5F5",
+    paper_bgcolor="#F5F5F5",
+)
+ 
+# create webpage layout 
+app.layout = html.Div(
+    style={"fontFamily": "Roboto, sans-serif", "backgroundColor": "#F5F5F5", "padding": "20px"},
+    children=[
+      # create banner for logo
+        html.H1(
+            "نظرة عامة على اتجاهات التوظيف",
+            style={"textAlign": "center", "color": "#333333"}
+        ),
+        html.Div(
+            [
+         
+                html.Div(
+                    dcc.Graph(figure=fig),
+                    style={"width": "48%", "display": "inline-block"}
+                ),
+            ],
+            style={"display": "flex", "justifyContent": "space-between"}
+        )
+    ]
+)
 
 if __name__ == '__main__':
   app.run(debug=True)
